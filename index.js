3dd9d6a901a936ad238a8ca7a75593ed6855c664
P2P-J <olnn712@gmail.com>
2025-09-28 02:18:54 +0900

    bg : 0과 소수점 연산 처리.

--- index.js
+++ index.js
@@ -1,85 +1,163 @@
-const calculator = document.getElementById('calculator')
-const keypad = calculator.querySelector('#keypad')
-const displayGroup = calculator.querySelector('#display-group')
-const expression = displayGroup.querySelector('#expression')
-let result = displayGroup.querySelector('#result')
-const allClear = keypad.querySelector('#all-clear')
+const calculator = document.getElementById("calculator");
+const keypad = calculator.querySelector("#keypad");
+const displayGroup = calculator.querySelector("#display-group");
+const expression = displayGroup.querySelector("#expression");
+let result = displayGroup.querySelector("#result");
+const allClear = keypad.querySelector("#all-clear");
 
 const initializeEvents = () => {
   // 키보드 이벤트
-  document.addEventListener('keydown', onKeyDown)
-  document.addEventListener('keyup', onKeyUp)
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
   // 마우스 이벤트
-  keypad.addEventListener('mousedown', onMouseDown)
-  keypad.addEventListener('mouseup', onMouseUp)
-}
+  keypad.addEventListener("mousedown", onMouseDown);
+  keypad.addEventListener("mouseup", onMouseUp);
+};
+
+let input = "";
 
 const onKeyDown = (event) => {
-  keypad.querySelector(`[data-code=${event.code}]`)?.classList.add('active')
-}
+  const keyButton = keypad.querySelector(`[data-code=${event.code}]`);
+  const key = keyButton?.dataset.key;
 
-const onKeyUp = (event) => {
-  keypad.querySelector(`[data-code=${event.code}]`)?.classList.remove('active')
-}
+  let keys = event.key;
 
-const onMouseDown = (event) => {
-  event.target.closest('.key')?.classList.add('active')
-}
+  if (event.shiftKey && event.code === "Digit8") keys = "*";
+  if (event.shiftKey && event.code === "Equal") keys = "+";
 
-let input = ''
-
-const onMouseUp = (event) => {
-  const keyButton = event.target.closest('button.key')
-
-  const key = keyButton?.dataset.key
-  const code = keyButton?.dataset.code
-
-  switch (code) {
-    case 'Allclear':
-      result.innerHTML = '0'
-      expression.innerHTML = ''
-      input = ''
-      break
-    case 'Backspace':
-      input = input.slice(0, -1)
-      result.innerHTML = input
-      break
-    case 'Equal':
-      const postfix = convertPostfix(input)
-      // console.log('후위표현식 : ', postfix)
-      const infix = convertInfix(postfix)
-      // console.log('중위표현식 : ', infix)
-
-      expression.innerHTML = input
-      result.innerHTML = Number(infix).toLocaleString('ko-KR')
-      input = ''
-      break
-    default:
-      input += key
-      // console.log(input)
-      result.innerHTML = input
-      break
+  if (event.code.startsWith("Digit")) {
+    input += key;
+    result.innerHTML = input;
   }
 
-  keypad.querySelector('.active')?.classList.remove('active')
-}
+  if (event.code === "Equal" || event.code === "Enter") {
+    if (input === "") {
+      return;
+    }
+    const postfix = convertPostfix(input);
+    // console.log('후위표현식 : ', postfix)
+    const infix = convertInfix(postfix);
+    // console.log('중위표현식 : ', infix)
+
+    expression.innerHTML = input;
+    result.innerHTML = Number(infix).toLocaleString("ko-KR");
+    input = result.textContent;
+  }
+
+  if (event.code === "Backspace") {
+    if (input.length > 0) {
+      input = input.slice(0, -1);
+      result.innerHTML = input === "" ? "0" : input;
+    } else {
+      result.innerHTML = "0";
+    }
+  }
+
+  keypad.querySelector(`[data-code=${event.code}]`)?.classList.add("active");
+};
+
+const onKeyUp = (event) => {
+  keypad.querySelector(`[data-code=${event.code}]`)?.classList.remove("active");
+};
+
+const onMouseDown = (event) => {
+  event.target.closest(".key")?.classList.add("active");
+};
+
+const onMouseUp = (event) => {
+  const keyButton = event.target.closest("button.key");
+
+  const key = keyButton?.dataset.key;
+  const code = keyButton?.dataset.code;
+
+  const lastChar = input[input.length - 1];
+  const operators = ["+", "-", "*", "/"];
+
+  switch (code) {
+    case "Allclear":
+      result.innerHTML = "0";
+      expression.innerHTML = "";
+      input = "";
+      break;
+    case "Backspace":
+      if (input.length > 0) {
+        input = input.slice(0, -1);
+        result.innerHTML = input === "" ? "0" : input;
+      } else {
+        result.innerHTML = "0";
+      }
+
+      break;
+    case "Equal":
+      if (input === "") {
+        break;
+      }
+      const postfix = convertPostfix(input);
+      // console.log('후위표현식 : ', postfix)
+      const infix = convertInfix(postfix);
+      // console.log('중위표현식 : ', infix)
+
+      expression.innerHTML = input;
+      result.innerHTML = Number(infix).toLocaleString("ko-KR");
+      input = result.textContent;
+      break;
+    default:
+      if (input === "" && operators.includes(key)) {
+        input = "0" + key;
+        result.innerHTML = input;
+        break;
+      }
+
+      if (operators.includes(lastChar) && operators.includes(key)) break;
+
+      // 소수점 연속 입력 방지
+      if (lastChar === "." && key === ".") break;
+
+      // 현재 숫자에 소수점이 이미 있으면 방지
+      if (key === ".") {
+        const nums = input.split(/[+\-*/]/);
+        if (nums[nums.length - 1].includes(".")) break;
+      }
+
+      if (key === "0") {
+        if (input === "0") break; //처음에 00 방지
+        const nums = input.split(/[+\-*/]/);
+        const zero = nums[nums.length - 1];
+        if (zero === "0") break;
+      }
+
+      if (key === ".") {
+        if (input === "" || operators.includes(lastChar)) {
+          input += "0.";
+          result.innerHTML = input;
+          break;
+        }
+      }
+
+      input += key;
+      result.innerHTML = input;
+      break;
+  }
+
+  keypad.querySelector(".active")?.classList.remove("active");
+};
 
 // 1. 연산자 우선순위 비교 함수
 const operatorPrecedence = (operator) => {
-  if (operator === '+' || operator === '-') return 1
-  if (operator === '*' || operator === '/') return 2
-  return 0
-}
+  if (operator === "+" || operator === "-") return 1;
+  if (operator === "*" || operator === "/") return 2;
+  return 0;
+};
 
 // 2. 중위표현식을 후위표현식으로 변환해주는 함수
 // 10*10+10000/10 => 1010*1000010/+
 // 20.3 * 5.6 + 4.75 / 10 => 20.3 5.6 * 4.75 10 / +
 const convertPostfix = (expression) => {
-  const operators = ['*', '/', '+', '-']
-  const stack = []
-  const postfix = []
+  const operators = ["*", "/", "+", "-"];
+  const stack = [];
+  const postfix = [];
 
-  const infix = expression.match(/(\d+(\.\d+)?|[+\-*/])/g)
+  const infix = expression.match(/(\d+(\.\d+)?|[+\-*/])/g);
 
   infix.forEach((token) => {
     if (operators.includes(token)) {
@@ -87,47 +165,47 @@ const convertPostfix = (expression) => {
         stack.length &&
         operatorPrecedence(stack[stack.length - 1]) >= operatorPrecedence(token)
       ) {
-        postfix.push(stack.pop())
+        postfix.push(stack.pop());
       }
-      stack.push(token)
+      stack.push(token);
     } else {
-      postfix.push(token)
+      postfix.push(token);
     }
-  })
+  });
 
   while (stack.length) {
-    postfix.push(stack.pop())
+    postfix.push(stack.pop());
   }
 
-  return postfix.join(' ')
-}
+  return postfix.join(" ");
+};
 
 // 후위표현식을 중위표현식으로 변환 후 계산해주는 함수
 // 10 10 * 10000 10 / + => 10*10+10000/10
 const convertInfix = (expression) => {
-  const operators = ['*', '/', '+', '-']
-  const stack = []
+  const operators = ["*", "/", "+", "-"];
+  const stack = [];
 
-  expression.split(' ').forEach((token) => {
+  expression.split(" ").forEach((token) => {
     if (operators.includes(token)) {
-      let preOperand = parseFloat(stack.pop())
-      let postOperand = parseFloat(stack.pop())
-      let temp
+      let preOperand = parseFloat(stack.pop());
+      let postOperand = parseFloat(stack.pop());
+      let temp;
 
-      if (token === '+') temp = postOperand + preOperand
-      else if (token === '-') temp = postOperand - preOperand
-      else if (token === '*') temp = postOperand * preOperand
-      else if (token === '/') temp = postOperand / preOperand
+      if (token === "+") temp = postOperand + preOperand;
+      else if (token === "-") temp = postOperand - preOperand;
+      else if (token === "*") temp = postOperand * preOperand;
+      else if (token === "/") temp = postOperand / preOperand;
 
-      stack.push(temp)
+      stack.push(temp);
     } else {
-      stack.push(token)
+      stack.push(token);
     }
-  })
+  });
 
-  console.log(stack)
+  console.log(stack);
 
-  return stack[0]
-}
+  return stack[0];
+};
 
-initializeEvents()
+initializeEvents();
